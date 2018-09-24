694974a22cd80286694d5b8f0b1a95d905776e33
lgserrania <a12026@bcc.unifal-mg.edu.br>
2018-09-24 16:05:39 -0300

    Inicio da leitura do arquivo

--- APA4/src/apa4/APA4.java
+++ APA4/src/apa4/APA4.java
@@ -5,6 +5,10 @@
  */
 package apa4;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
@@ -17,7 +21,7 @@ public class APA4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        formaHorario fh = new formaHorario();
     }
     
    
--- APA4/src/apa4/formaHorario.java
+++ APA4/src/apa4/formaHorario.java
@@ -5,8 +5,12 @@
  */
 package apa4;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,12 +18,50 @@ import java.io.FileReader;
  */
 public class formaHorario {
     
-    formaHorario(){
+    private int numProf;
+    private int numTurmas;
+    private String[] turmas;
+    private int numDias;
+    private int numHorarios;
+    
+    public formaHorario(){
+        
+        try {
+            lerArquivo();
+        } catch (IOException ex) {
+            Logger.getLogger(formaHorario.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
-    private void lerArquivo() throws FileNotFoundException{
+    public void lerArquivo() throws FileNotFoundException, IOException{
         FileReader arq = new FileReader("nilda36.txt");
+        BufferedReader lerArq = new BufferedReader(arq);
+        
+        //Pega o número de professores
+        String linha = lerArq.readLine();
+        String[] linhaPart = linha.split("\t");
+        this.numProf = Integer.parseInt(linhaPart[0]);
+        
+        //Pega o número de turmas
+        linha = lerArq.readLine();
+        linhaPart = linha.split("\t");
+        this.numTurmas = Integer.parseInt(linhaPart[0]);
+        
+        //Cria o vetor com o nome das turmas
+        this.turmas = new String[this.numTurmas];
+        
+        //Preenche o vetor com o nome das turmas
+        for(int i = 1; i < linhaPart.length - 1; i++){
+            this.turmas[i] = linhaPart[i];
+        }
+        
+        //Pega o número de dias e horários
+        linha = lerArq.readLine();
+        linhaPart = linha.split("\t");
+        this.numDias = Integer.parseInt(linhaPart[0]);
+        this.numHorarios = Integer.parseInt(linhaPart[1]);
+        
     }
     
 }
