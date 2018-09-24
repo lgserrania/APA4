eb1b49a02c4bf5427fa2d4e6fe8cfcde2140776a
lgserrania <a12026@bcc.unifal-mg.edu.br>
2018-09-24 17:21:57 -0300

    Alocacao Inicial

--- APA4/src/apa4/formaHorario.java
+++ APA4/src/apa4/formaHorario.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,25 +17,49 @@ import java.util.logging.Logger;
  *
  * @author 2017.1.08.008
  */
-public class formaHorario {
+public final class formaHorario {
     
     private int numProf;
     private int numTurmas;
     private String[] turmas;
     private int numDias;
     private int numHorarios;
+    private int[][][] mati; //Matriz de Indisponibilidade
+    private int[][] mata; //Matriz de Aulas
+    private int[][] profHorasDist; //Matriz com as horas já distribuidas do professor pra certa turma
+    private int[][][] profHorarios;
+    private int[][][] horarioTurmas;
+   
+    
     
     public formaHorario(){
         
         try {
-            lerArquivo();
+            this.lerArquivo();
         } catch (IOException ex) {
             Logger.getLogger(formaHorario.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+        this.horarioTurmas = new int[this.numTurmas][this.numDias][this.numHorarios];
+        
+        for(int i = 0; i < this.numTurmas; i++){
+            for(int j = 0; j < this.numDias; j++){
+                for(int k = 0; k < this.numHorarios; k++){
+                    this.horarioTurmas[i][j][k] = -1;
+                }
+            }
+        }
+        
+        this.alocacao();
+        
+        this.escreveHorarios();
+        
+        //this.escreveHorasDistribuidas();
+        
     }
     
     public void lerArquivo() throws FileNotFoundException, IOException{
+        
         FileReader arq = new FileReader("nilda36.txt");
         BufferedReader lerArq = new BufferedReader(arq);
         
@@ -62,6 +87,103 @@ public class formaHorario {
         this.numDias = Integer.parseInt(linhaPart[0]);
         this.numHorarios = Integer.parseInt(linhaPart[1]);
         
+        //Inicializando variáveis
+        this.profHorarios = new int[this.numProf][this.numDias][this.numHorarios];
+        this.profHorasDist = new int[this.numProf][this.numTurmas];
+         
+        //Gerando a matriz de indisponibilidade
+        this.mati = new int[this.numProf][this.numDias][this.numHorarios];
+        
+        linha = lerArq.readLine();
+        linhaPart = linha.split("\t");
+        
+        //Os horários em que o professor não está disponível
+        //Serão preenchidos com o valor 1
+        //Os disponíveis terão o valor 0
+        while("i".equals(linhaPart[0])){
+            int dia = Integer.parseInt(linhaPart[1]);
+            int horario = Integer.parseInt(linhaPart[2]);
+            int prof = Integer.parseInt(linhaPart[3]);
+            
+            this.mati[prof][dia][horario] = 1;
+            linha = lerArq.readLine();
+            linhaPart = linha.split("\t");
+        }
+        
+        //Gerando a matriz de aulas
+        this.mata = new int[this.numProf][this.numTurmas];
+        
+        linha = lerArq.readLine();
+        
+        //As linhas são os professores, as colunas são as turmas
+        //e o valor é o número de horas que ele precisa dar
+        while(linha != null){          
+            linhaPart = linha.split("\t");
+            int prof = Integer.parseInt(linhaPart[1]);
+            int turma = Integer.parseInt(linhaPart[2]);
+            this.mata[prof][turma] = Integer.parseInt(linhaPart[3]);
+            
+            linha = lerArq.readLine();   
+        }
+        
+        arq.close();
+
     }
     
+    public void alocacao(){
+        
+        Random gerador = new Random();
+        
+        for(int i = 0; i < 1000000; i++){
+            
+            int dia = gerador.nextInt(this.numDias);
+            int turma = gerador.nextInt(this.numTurmas);
+            int horario = gerador.nextInt(this.numHorarios);
+           
+            int prof = gerador.nextInt(this.numProf);
+                
+            if(this.mati[prof][dia][horario] != 1){
+                if(this.profHorasDist[prof][turma] < this.mata[prof][turma]){
+                    if(this.profHorarios[prof][dia][horario] != 1){
+                        this.horarioTurmas[turma][dia][horario] = prof;
+                        this.profHorarios[prof][dia][horario] = 1;
+                        this.profHorasDist[prof][turma]++;
+                    }
+                }
+            }
+        }    
+    }
+    
+    private void escreveHorarios(){
+        
+        for(int i = 0; i < this.numTurmas; i++){
+            System.out.println("########### TURMA " + i + " ###########");
+            for(int j = 0; j < this.numDias; j++){
+                System.out.print("Dia " + j + " -> ");
+                System.out.print(this.horarioTurmas[i][j][0]);
+                for(int k = 1; k < this.numHorarios; k++){
+                    System.out.print(" - " + this.horarioTurmas[i][j][j]);
+                }
+                System.out.println("");
+            }
+        }
+        
+    }
+    
+    private void escreveHorasDistribuidas(){
+        
+        for(int i = 0; i < this.numProf; i++){
+            System.out.println("############# Professor " + i + " #############");
+            for(int j = 0; j < this.numTurmas; j++){
+                System.out.println("Turma " + j + ": " + this.profHorasDist[i][j] + " horas");
+                if(this.profHorasDist[i][j] == this.mata[i][j]){
+                    System.out.println("Quantidade certa");
+                }else if(this.profHorasDist[i][j] < this.mata[i][j]){
+                    System.out.println("Faltou horas");
+                }else{
+                    System.out.println("Passou horas");
+                }
+            }
+        }
+    }
 }
