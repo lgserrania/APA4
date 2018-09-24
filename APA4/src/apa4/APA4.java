a124ae1573a211f38b1274a75f0f6ce82f4b8e64
lgserrania <a12026@bcc.unifal-mg.edu.br>
2018-09-24 15:48:27 -0300

    first commit

---
+++ APA4/src/apa4/APA4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apa4;
+
+
+/**
+ *
+ * @author 2017.1.08.008
+ */
+public class APA4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+   
+    
+}
---
+++ APA4/src/apa4/formaHorario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apa4;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ *
+ * @author 2017.1.08.008
+ */
+public class formaHorario {
+    
+    formaHorario(){
+        
+    }
+    
+    private void lerArquivo() throws FileNotFoundException{
+        FileReader arq = new FileReader("nilda36.txt");
+    }
+    
+}
